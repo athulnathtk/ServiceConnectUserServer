c9dc0fce59fda9606f91679ab59ea2c22740489e
Athul Nath <tkathulnath@gmail.com>
2024-11-08 02:00:27 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log("MongoDB connected...");
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log("MongoDB connected..");
     } catch (error) {
-      console.err("MongoDB connection failed: ", err.message);
+      console.error("MongoDB connection error: ", error.message);
       process.exit(1);
     }
   };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/AuthRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -7,7 +8,9 @@ app.use(express.json());
 
 connectDB();
 
+app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
